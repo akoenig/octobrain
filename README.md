95a526af62d3d399a08eee35af1e2306a18b2468
André König <andre.koenig@posteo.de>
2014-06-06 09:58:37 +0200

    Initial commit. Created directory structure.

---
+++ README.md
@@ -0,0 +1,15 @@
+# octobrain
+
+Helps you to avoid typing GitHub URIs over and over again.
+
+## Installation
+
+Install with [npm](https://npmjs.org/package/octobrain).
+
+    npm install --save octobrain
+
+## Changelog
+
+## Author
+
+Copyright 2014, [André König](http://) (andre.koenig@posteo.de)
---
+++ specs/index.spec.js
@@ -0,0 +1,21 @@
+/*
+ * octobrain
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+describe('The "octobrain"', function () {
+
+    it('should ...', function (done) {
+        done();
+    });
+});
